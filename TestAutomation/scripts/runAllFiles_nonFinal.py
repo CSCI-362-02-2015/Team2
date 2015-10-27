e68bee3d92f61ff17565362a3c64bc8abce4be14
James Keels <keelsjo@gmail.com>
2015-10-26 22:04:46 -0400

    intermediate CheckIn

--- TestAutomation/scripts/runAllFiles_nonFinal.py
+++ TestAutomation/scripts/runAllFiles_nonFinal.py
@@ -1,54 +1,137 @@
+#import statements
+import os
+import webbrowser
+import json
+from pprint import pprint
+
+
 def main():
-    saveFile = "testCaseResults"
-    appendToHtmlStr = ''
+    saveFile = "testCaseResults.html"
+    htmlStr = ''
     
-    createResults(saveFile)
     list_testFileNames = getTests()
     list_oracleFileNames = getOracles()
-    runTestCase(list_testFileNames)
-    appendToHtml(appendToHtmlStr)
+    runTestCase(list_testFileNames, list_oraclsFileNames)
+    createResults(saveFile)
 
 def createResults(saveFile):
 
     outFile = open(saveFile, 'w')
 
     outFile.write(
-            "<!DOCTYPE html>" +
-            "<html>" +
-                "<head>" +
-                    "<title>Page Title</title>" +
-                    "<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.4/flatly/bootstrap.min.css" />" +
-                    "<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />" +
-                    "<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>" +
-                    "<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>" +
-                "</head>" +
-                "<body></body>" +
-            "</html>")
+            '<!DOCTYPE html>' +
+            '<html>' +
+                '<head>' +
+                    '<title>Page Title</title>' +
+                    '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.4/flatly/bootstrap.min.css" />' +
+                    '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" />' +
+                    '<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>' +
+                    '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>' +
+                '</head>' +
+                '<body>' +
+                    '<div class="container">' +
+                        '<div id="top"></div>' +
+			'<div id="mid">' +
+                            '<div class="accordion-group">' +
+                                '<div class="accordion-heading">' +
+                                    '<a class="">' +
+                                        '<div class="row center">' +
+                                            '<div class="col-lg-2">' +
+                                                '<h5 id="heading_id">ID</h5>' +
+					    '</div>' +
+					    '<div class="col-lg-7">' +
+                                                '<h5 id="heading_title">Title</h5>' +
+					    '</div>' +
+					    '<div class="col-lg-3">' +
+                                                '<h5 id="heading_status">Status</h5>' +
+					    '</div>' +
+					'</div>' +
+				    '</a>' +
+                                '</div>' +
+                                '<div id="aclDetailsCollapse" class="accordion-body">' +
+                                    htmlStr +
+                                '</div>' +
+                            '</div>' +
+                        '</div>' +
+                        '<div id="bottom"></div>' +
+                    '</div>' +
+                '</body>' +
+            '</html>')
 
 def getTests():
-    f
+    '''get current directory'''	
+    str_dirPath = os.path.dirname(os.path.realpath(__file__))
+
+    '''get top parent'''
+    str_topParent = str_dirPath[:(str_dirPath.index("scripts"))] + "testCases"
+
+    '''get files in parent directory as a comma delimited list'''
+    return os.listdir(str_topParent)
     
 def getOracles():
-    f
-    
-def runTestCase(fileNameList):
-    for(fileName in fileNameList):
-        result = doTest(fileName)
+    '''get current directory'''	
+    str_dirPath = os.path.dirname(os.path.realpath(__file__))
 
-def doTests(fileName):
+    '''get top parent'''
+    str_topParent = str_dirPath[:(str_dirPath.index("scripts"))] + "oracles"
+
+    '''get files in parent directory as a comma delimited list'''
+    return os.listdir(str_topParent)
+    
+def runTestCase(fileNameList, oracleList):
+    for fileName, oracleName in zip(fileNameList, oracleList):
+        result = doTest(fileName, oracleName)
+
+def doTests(fileName, oracleName):
     tc_id = ''
     tc_title = ''
     tc_req = ''
     tc_testVal = ''
     tc_oracle = ''
+    tc_result = ''
     
     infile = open(fileName, 'r')
     #parse JSON file
 
-    appendToHtmlStr += 
-    
-def appendToHtml(appendStr):
-    f
+    htmlStr += '<div class="accordion-inner" id="tc_{0}">' +
+                            '<div class="accordion" id="tcAccordion{0}">' +
+                                '<div class="accordion-group">' +
+                                    '<div class="accordion-heading">' +
+                                        '<a class="accordion-toggle" data-toggle="collapse" data-parent="#tcAccordion{0)" href="#tcDetailsPanel{0}">' +
+                                            '<div class="row">' +
+                                                '<div class="col-lg-2">' +
+                                                    '<p id="tc_id{0}">{0}</p>' +
+                                                '</div>' +
+                                                '<div class="col-lg-7">' +
+                                                    '<p id="tc_title{0}">{1}</p>' +
+                                                '</div>' +
+                                                '<div class="col-lg-3">' +
+                                                    '<p id="tc_status{0}">{2}</p>' +
+                                                '</div>' +
+                                            '</div>' +
+                                        '</a>' +
+                                    '</div>' +
+                                    '<div id="tcDetailsPanel{0}" class="accordion-body collapse">' +
+                                        '<div class="accordion-inner divShading-beige" id="tcDetails{0}">' +
+                                            '<div class="row">' +
+                                                '<div class="col-lg-12">' +
+                                                    '<p id="tc_req{0}"><strong>Requirement: </strong>{3}</p>' +
+                                                '</div>' +
+                                            '</div>' +
+                                            '<div class="row">' +
+                                                '<div class="col-lg-3 col-lg-offset-3">' +
+                                                    '<p id="tc_testVal{0}"><strong>Test Value: </strong>{4}</p>' +
+                                                '</div>' +
+                                                '<div class="col-lg-3">' +
+                                                    '<p id="tc_oracle{0}"><strong>Oracle: </strong>{5}</p>' +
+                                                '</div>' +
+                                            '</div>' +
+                                        '</div>' +
+                                    '</div>' +
+                                '</div>' +
+                            '</div>' +
+                        '</div>'.format(tc_id, tc_title, tc_result, tc_req, tc_testVal, tc_oracle)
+
     
         
         
