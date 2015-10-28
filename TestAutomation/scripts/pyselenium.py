26ccf299575f2edf7c429875eb0a1d9756cff56c
Charlie <ctt3>
2015-10-28 18:41:29 -0400

    selenium works, baby steps to glory

---
+++ TestAutomation/scripts/pyselenium.py
@@ -0,0 +1,38 @@
+from selenium import webdriver # pip install selenium
+from selenium.webdriver.common.keys import Keys
+from PIL import Image
+import os, time
+
+
+def wait_until_load():
+	def page_has_loaded(): return driver.execute_script('return document.readyState;') == 'complete'
+	time.sleep(1)
+	count = 0
+	while not(page_has_loaded() or count==10):
+		time.sleep(0.25)
+		count+=1
+
+testcases = {"testCase1":"x = {-b \pm \sqrt{b^2-4ac} \over 2a}", "testCase2":"x = 5/3", "testCase3":"x = {-b \pm \sqrt{b^5-4acdef} \over 2ba}"}
+
+driver = webdriver.Firefox()
+parent_dir_path = "%s/../" %os.path.dirname(os.path.abspath(__file__))
+skeleton_file_path = parent_dir_path + "project/src/skeleton.html"
+driver.get("file:///" + skeleton_file_path) #navigate to skeleton file
+wait_until_load()
+
+for testcase_name in testcases.keys():
+	oracle_path = parent_dir_path + "oracles/%s.jpg" % testcase_name
+	result_path = parent_dir_path + "temp/testResults/%s.jpg" % testcase_name
+	driver.find_element_by_id("MathInput").clear() #clear input box
+	driver.find_element_by_id("MathInput").send_keys(testcases[testcase_name]) #type testcase into box
+	driver.find_element_by_id("MathInput").send_keys(Keys.RETURN) #hit enter
+	wait_until_load()
+	driver.get_screenshot_as_file(oracle_path) #save screenshot (oracle_path to save oracles)
+	# driver.get_screenshot_as_file(result_path) #save screenshot (result_path to test)
+
+driver.quit()
+
+# IMAGE COMPARISON
+# import ImageChops
+# def equal(im1, im2):
+#     return ImageChops.difference(im1, im2).getbbox() is None
